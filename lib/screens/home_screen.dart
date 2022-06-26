2f8db5e927b019b232697f64cc2b405e25aabf53
TonyNKS <tony.nks@outlook.com>
2022-06-26 17:31:41 +0500

    добавил ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -34,8 +34,10 @@ class _HomeScreenState extends State<HomeScreen> {
                       child: Text(
                         'My Diary',
                         style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 30, color: Color.fromRGBO(55, 18, 100,  1),
-                        fontFamily: "BalooChettan2"),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                            color: Color.fromRGBO(55, 18, 100, 1),
+                            fontFamily: "BalooChettan2"),
                       ),
                     ),
                     Row(
@@ -43,98 +45,174 @@ class _HomeScreenState extends State<HomeScreen> {
                       children: [
                         Icon(
                           Icons.arrow_back_ios,
-                          size: 16, color: Color.fromRGBO(55, 18, 100,  1),
+                          size: 16,
+                          color: Color.fromRGBO(55, 18, 100, 1),
                         ),
                         Row(
                           children: [
-                            Icon(
-                              Icons.calendar_today_outlined,
-                              size: 14, color: Color.fromRGBO(55, 18, 100,  1)
+                            Icon(Icons.calendar_today_outlined,
+                                size: 14,
+                                color: Color.fromRGBO(55, 18, 100, 1)),
+                            SizedBox(
+                              width: 10,
                             ),
-                            SizedBox(width: 10,),
                             Text(
                               '15 May',
-                              style: TextStyle(fontSize: 14, color: Color.fromRGBO(55, 18, 100,  1), fontFamily: "BalooChettan2", fontWeight: FontWeight.bold),
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Color.fromRGBO(55, 18, 100, 1),
+                                  fontFamily: "BalooChettan2",
+                                  fontWeight: FontWeight.bold),
                             ),
                           ],
                         ),
-                        Icon(
-                          Icons.arrow_forward_ios,
-                          size: 16, color: Color.fromRGBO(55, 18, 100,  1)
-                        ),
+                        Icon(Icons.arrow_forward_ios,
+                            size: 16, color: Color.fromRGBO(55, 18, 100, 1)),
                       ],
                     ),
                   ],
                 ),
               ),
-
-              // Текст Mediterranean diet - details
-              Padding(
-                padding: const EdgeInsets.only(left: 28.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 10),
-                      child: Text('Mediterranean diet', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Color.fromRGBO(55, 18, 100,  1), fontFamily: "BalooChettan2",),),
-                    ),
-                    Expanded(
-                      child: Row(
-                        children: [
-                          SizedBox(width: 90,),
-                          Text('Details', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14, color: Colors.blue, fontFamily: "BalooChettan2",),),
-                          SizedBox(width: 10,),
-                          Icon(Icons.arrow_forward,  size: 16, color: Colors.blueGrey),
-                        ],
+              Expanded(
+                child: SizedBox(
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    children: [
+                      // Текст Mediterranean diet - details
+                      Padding(
+                        padding: const EdgeInsets.only(left: 28.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 5.0, vertical: 10),
+                              child: Text(
+                                'Mediterranean diet',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Color.fromRGBO(55, 18, 100, 1),
+                                  fontFamily: "BalooChettan2",
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  SizedBox(
+                                    width: 90,
+                                  ),
+                                  Text(
+                                    'Details',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                      color: Colors.blue,
+                                      fontFamily: "BalooChettan2",
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Icon(Icons.arrow_forward,
+                                      size: 16, color: Colors.blueGrey),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              // Статистика какая-то
-              Padding(
-                padding: const EdgeInsets.only(left: 28.0),
-                child: Statistics(),
-              ),
-              SizedBox(height: 20),
-
-              // Текст Meals today - Customize
-              Padding(
-                padding: const EdgeInsets.only(left: 28.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 10),
-                      child: Text('Meals today', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Color.fromRGBO(55, 18, 100,  1), fontFamily: "BalooChettan2",),),
-                    ),
-                    Container(
-                      child: Row(
-                        children: [
-                          Text('Customize', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14, color: Colors.blue, fontFamily: "BalooChettan2",),),
-                          SizedBox(width: 10,),
-                          Icon(Icons.arrow_forward,  size: 16, color: Colors.blueGrey),
-                          SizedBox(width: 30,)
-                        ],
+                      // Статистика какая-то
+                      Padding(
+                        padding: const EdgeInsets.only(left: 28.0, right: 28.0),
+                        child: Statistics(),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 10),
-              //Контейнеры красивые не понятные
-              Padding(
-                padding: const EdgeInsets.only(left: 28.0),
-                child: Row(
-                  children: [
-                    MealsToday(),
-                    MealsToday(),
-                    MealsToday(),
-                  ],
-                ),
-              ),
+                      SizedBox(height: 20),
 
-
+                      // Текст Meals today - Customize
+                      Padding(
+                        padding: const EdgeInsets.only(left: 28.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 5.0, vertical: 10),
+                              child: Text(
+                                'Meals today',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Color.fromRGBO(55, 18, 100, 1),
+                                  fontFamily: "BalooChettan2",
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Row(
+                                children: [
+                                  Text(
+                                    'Customize',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                      color: Colors.blue,
+                                      fontFamily: "BalooChettan2",
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Icon(Icons.arrow_forward,
+                                      size: 16, color: Colors.blueGrey),
+                                  SizedBox(
+                                    width: 30,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      //Контейнеры красивые не понятные
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: const [
+                            SizedBox(width: 30),
+                            MealsToday(
+                              full: true,
+                              title: 'Breakfast',
+                              image: 'assets/images/Breakfast.png',
+                              kcal: '525',
+                              startColor: Color.fromRGBO(250, 120, 130, 1),
+                              endColor: Color.fromRGBO(253, 170, 148, 1),
+                            ),
+                            MealsToday(
+                              full: true,
+                              title: 'Lunch',
+                              image: 'assets/images/Lunch.png',
+                              kcal: '602',
+                              startColor: Color.fromRGBO(116, 139, 232, 1),
+                              endColor: Color.fromRGBO(92, 96, 223, 1),
+                            ),
+                            MealsToday(
+                                full: true,
+                                image: 'assets/images/Snack.png',
+                                title: 'Snack',
+                                kcal: '800',
+                                startColor: Color.fromRGBO(254, 149, 181, 1),
+                                endColor: Color.fromRGBO(250, 84, 134, 1))
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
 
               //Body measurement - Today
             ],
